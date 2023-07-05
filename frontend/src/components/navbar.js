c1cbfcadd091475d41b5495b3717e6d4fcce16f0
JayMuppidi <jaikarsai.muppidi@gmail.com>
2023-07-05 00:24:49 -0500

    Signup/login page is almost done, but nav bar needs to be fixed so that it shows add a task button only after login. Toggle button between sign up and sign in button needs a bit more margin space.

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,165 @@
+import React from "react";
+import {
+  chakra,
+  Box,
+  Flex,
+  useColorModeValue,
+  VisuallyHidden,
+  HStack,
+  Button,
+  useDisclosure,
+  VStack,
+  IconButton,
+  CloseButton,
+  Avatar,
+  Center,
+  Text,
+  SimpleGrid,
+} from "@chakra-ui/react";
+import {
+  AiFillHome,
+  AiOutlineMenu,
+  AiOutlineBell,
+} from "react-icons/ai";
+import { BsFillCameraVideoFill, BsPlus } from "react-icons/bs";
+
+export default function Navbar() {
+  const bg = useColorModeValue("white", "brand.800");
+  const mobileNav = useDisclosure();
+
+  return (
+    <React.Fragment>
+      <chakra.header
+        bg={"brand.300"}
+        w="full"
+        px={{ base: 2, sm: 4 }}
+        py={4}
+        shadow="md"
+      >
+        <Flex alignItems="center" justifyContent="space-between" mx="auto">
+          <HStack display="flex" spacing={3} alignItems="center">
+            <Box display={{ base: "inline-flex", md: "none" }}>
+              <IconButton
+                display={{ base: "flex", md: "none" }}
+                aria-label="Open menu"
+                fontSize="20px"
+                color="gray.800"
+                _dark={{ color: "inherit" }}
+                variant="ghost"
+                icon={<AiOutlineMenu />}
+                onClick={mobileNav.onOpen}
+              />
+              <VStack
+                pos="absolute"
+                top={0}
+                left={0}
+                right={0}
+                display={mobileNav.isOpen ? "flex" : "none"}
+                flexDirection="column"
+                p={2}
+                pb={4}
+                m={2}
+                bg={bg}
+                spacing={3}
+                rounded="sm"
+                shadow="sm"
+              >
+                <CloseButton
+                  aria-label="Close menu"
+                  justifySelf="self-start"
+                  onClick={mobileNav.onClose}
+                />
+                <Button
+                  w="full"
+                  variant="ghost"
+                  leftIcon={<BsFillCameraVideoFill />}
+                >
+                  Videos
+                </Button>
+              </VStack>
+            </Box>
+            <chakra.a
+              href="/"
+              title="Choc Home Page"
+              display="flex"
+              alignItems="center"
+            >
+              <VisuallyHidden>Choc</VisuallyHidden>
+            </chakra.a>
+
+            <HStack spacing={3} display={{ base: "none", md: "inline-flex" }}>
+              <Button variant="ghost" leftIcon={<AiFillHome />} size="sm">
+                Dashboard
+              </Button>
+            </HStack>
+          </HStack>
+
+          <SimpleGrid flex="1" minChildWidth="120px" spacing="20px" justifyItems="center">
+            <Box
+              p={2}
+              bg="white"
+              rounded="md"
+              shadow="md"
+              display="flex"
+              alignItems="center"
+            >
+              <chakra.p
+                fontSize="xl"
+                fontWeight="bold"
+                color="gray.800"
+                _dark={{ color: "gray.200" }}
+                textTransform="uppercase"
+                letterSpacing="wider"
+                display="inline-flex"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Box
+                  as="span"
+                  bgGradient="linear(to-r, brand.500, brand.300)"
+                  bgClip="text"
+                  fontWeight="bold"
+                >
+                  T
+                </Box>
+                asks
+              </chakra.p>
+            </Box>
+          </SimpleGrid>
+
+          <HStack
+            spacing={3}
+            display={mobileNav.isOpen ? "none" : "flex"}
+            alignItems="center"
+          >
+            <Button
+              colorScheme="brand"
+              color="brand.50"
+              textColor="White"
+              leftIcon={<BsPlus />}
+            >
+              New Task
+            </Button>
+
+            <chakra.a
+              p={3}
+              color="gray.800"
+              _dark={{ color: "inherit" }}
+              rounded="sm"
+              _hover={{ color: "gray.800", _dark: { color: "gray.600" } }}
+            >
+              <AiOutlineBell />
+              <VisuallyHidden>Notifications</VisuallyHidden>
+            </chakra.a>
+
+            <Avatar
+              size="sm"
+              name="Dan Abrahmov"
+              src="https://bit.ly/dan-abramov"
+            />
+          </HStack>
+        </Flex>
+      </chakra.header>
+    </React.Fragment>
+  );
+}
---
+++ frontend/src/landingpage.js
@@ -0,0 +1,182 @@
+import React, { useState } from "react";
+import {
+  chakra,
+  Box,
+  GridItem,
+  Button,
+  Center,
+  Flex,
+  Icon,
+  SimpleGrid,
+  VisuallyHidden,
+  Input,
+  VStack,
+  ChakraBaseProvider,
+  Switch,
+} from "@chakra-ui/react";
+
+export default function App() {
+  const [isSignUp, setIsSignUp] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const handleToggleSignUp = () => {
+    setIsSignUp(!isSignUp);
+  };
+
+  const handleToggleAdmin = () => {
+    setIsAdmin(!isAdmin);
+  };
+
+  return (
+    <Box px={8} py={24} mx="auto" minHeight="100vh" bg="brand.50">
+      <VStack spacing={8} alignItems="center">
+        <GridItem
+          colSpan={{ base: "auto", lg: 7 }}
+          textAlign={{ base: "center", lg: "left" }}
+        >
+          <chakra.h1
+            mb={4}
+            fontSize={{ base: "3xl", md: "4xl" }}
+            fontWeight="bold"
+            lineHeight={{ base: "shorter", md: "none" }}
+            color="gray.900"
+            _dark={{ color: "gray.200" }}
+            letterSpacing={{ base: "normal", md: "tight" }}
+          >
+            Ready to increase your team's productivity?
+          </chakra.h1>
+        </GridItem>
+        <GridItem colSpan={{ base: "auto", md: 4 }}>
+          <Flex
+            as="form"
+            direction="column"
+            alignItems="center"
+            rounded="lg"
+            shadow="xl"
+            bg="brand.200"
+            px={8}
+            py={6}
+            maxW="md"
+            w="full"
+          >
+            {isSignUp ? (
+              <>
+                <VisuallyHidden>First Name</VisuallyHidden>
+                <Input
+                  my={2}
+                  type="text"
+                  placeholder="First Name"
+                  bg="white"
+                  _placeholder={{ color: "brand.500" }}
+                />
+
+                <VisuallyHidden>Last Name</VisuallyHidden>
+                <Input
+                  my={2}
+                  type="text"
+                  placeholder="Last Name"
+                  bg="white"
+                  _placeholder={{ color: "brand.500" }}
+                />
+
+                <VisuallyHidden>Email Address</VisuallyHidden>
+                <Input
+                  my={2}
+                  type="email"
+                  placeholder="Email Address"
+                  bg="white"
+                  _placeholder={{ color: "brand.500" }}
+                />
+
+                <VisuallyHidden>Password</VisuallyHidden>
+                <Input
+                  my={2}
+                  type="password"
+                  placeholder="Password"
+                  bg="white"
+                  _placeholder={{ color: "brand.500" }}
+                />
+
+                <Switch
+                  colorScheme="brand"
+                  isChecked={isAdmin}
+                  onChange={handleToggleAdmin}
+                  size="lg"
+                  mt={2}
+                  mb={4}
+                >
+                  {isAdmin ? "Admin" : "Normal User"}
+                </Switch>
+              </>
+            ) : (
+              <>
+                <VisuallyHidden>Email Address</VisuallyHidden>
+                <Input
+                  my={2}
+                  type="email"
+                  placeholder="Email Address"
+                  bg="white"
+                  _placeholder={{ color: "brand.500" }}
+                />
+
+                <VisuallyHidden>Password</VisuallyHidden>
+                <Input
+                  my={2}
+                  type="password"
+                  placeholder="Password"
+                  bg="white"
+                  _placeholder={{ color: "brand.500" }}
+                />
+              </>
+            )}
+
+            <Button
+              colorScheme="brand"
+              w="full"
+              py={3}
+              my={4}
+              type="submit"
+              bg="brand.500"
+              _hover={{ bg: "brand.600" }}
+            >
+              {isSignUp ? "Sign up for free" : "Sign in"}
+            </Button>
+
+            <Button
+              py={3}
+              w="full"
+              colorScheme="blue"
+              leftIcon={
+                <Icon
+                  mr={2}
+                  aria-hidden="true"
+                  boxSize={6}
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  stroke="transparent"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M20.283,10.356h-8.327v3.451h4.792c-0.446,2.193-2.313,3.453-4.792,3.453c-2.923,0-5.279-2.356-5.279-5.28	c0-2.923,2.356-5.279,5.279-5.279c1.259,0,2.397,0.447,3.29,1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233	c-4.954,0-8.934,3.979-8.934,8.934c0,4.955,3.979,8.934,8.934,8.934c4.467,0,8.529-3.249,8.529-8.934	C20.485,11.453,20.404,10.884,20.283,10.356z" />
+                </Icon>
+              }
+              bg="blue"
+              _hover={{ bg: "blue.100" }}
+            >
+              Login using Google
+            </Button>
+
+            <Button
+              variant="link"
+              colorScheme="brand"
+              onClick={handleToggleSignUp}
+            >
+              {isSignUp ? "Sign in instead" : "Create an account"}
+            </Button>
+          </Flex>
+        </GridItem>
+      </VStack>
+    </Box>
+  );
+}
